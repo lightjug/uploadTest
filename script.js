da5c39c9f4ce76ea2009678db379768f2897b096
lightjug <lightkyoro+git@gmail.com>
2015-05-11 15:53:22 +0900

    最初のコミット

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('TabController',function($scope){
+    $scope.tabList = [
+		{text:"メイン",userID:""}
+		,{text:"タブ１",userID:"user1"}
+		,{text:"タブ２",userID:"user2"}
+		,{text:"タブ３",userID:"user3"}
+	];
+});
+
+$(function(){
+    $('#tabArea').sortable();
+});
+
